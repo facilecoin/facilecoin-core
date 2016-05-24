6304dbda7315a288ad97d0ff0be1b87abd402367
facilecoin <info@facilecoin.org>
2016-05-25 02:59:31 +0300

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -56,7 +56,7 @@ const std::string CLIENT_NAME("Satoshi");
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj, min, rev, build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-rc2"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-rc2-02"
 
 //#define BUILD_SUFFIX m	
 #ifndef BUILD_DESC
